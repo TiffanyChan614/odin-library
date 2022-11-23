a945d26941cc045fef038e37d45b3e3373fe8598
TiffChan <tiffanychan1999614@gmail.com>
2022-11-23 01:20:42 -0500

    Feat: Allow user to remove books

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function Book(name, author, pages, read) {
 
 function showBookForm() {
     const form = document.querySelector(".form-window");
-    form.style.display = "block";
+    form.style.visibility = "visible";
 }
 
 function hideBookForm() {
     const form = document.querySelector(".form-window");
-    form.style.display = "none";
+    form.style.visibility = "none";
 }
 
 function addBookToLibrary(myLibrary) {
@@ -32,49 +32,88 @@ function getNewBook() {
     return new Book(name, author, pages, read);
 }
 
+function displayBooks(myLibrary) {
+    console.log("I have " + myLibrary.length + " books");
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        console.log(i, book.name);
+        genDiv(".book-container", "book", `book${i}`);
+        genDiv(`#book${i}`, "book-info", `book${i}-info`);
+        genDiv(`#book${i}`, "book-action", `book${i}-action`)
+        genBookInfo(book, `#book${i}-info`);
+        genBookAction(`#book${i}-action`, i);
+    }
+}
+
+function genDiv(container, className, element_ID) {
+    const div_container = document.querySelector(container);
+    let div = document.createElement("div");
+    div.className = className;
+    div.id = element_ID;
+    div_container.appendChild(div);
+}
+
+function genBookInfo(book, book_div_ID) {
+    const book_div = document.querySelector(book_div_ID);
+    const book_info = [book.name, book.author, book.pages, book.read];
+    for (let j = 0; j < 4; j++){
+        let p = document.createElement("p");
+        p.textContent = book_info[j];
+        book_div.appendChild(p);
+    }
+}
+
+function genBookAction(book_div_ID, book_index) {
+    const book_div = document.querySelector(book_div_ID);
+    let btn = document.createElement("button");
+    btn.type = "button";
+    btn.className = "remove";
+    btn.id = `remove-${book_index}`;
+    btn.textContent = "Remove";
+    book_div.appendChild(btn);
+    document.querySelectorAll(".remove").forEach(btn =>
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            console.log('click');
+            const book = btn.parentElement;
+            const book_container = book.parentElement;
+            let bookID = book.id;
+            let book_index = bookID.replace(/[^0-9]+/gi, '');
+            book_container.remove(book);
+            removeBook(myLibrary, book_index);
+    }));
+}
+
+function clearBox(element_ID) {
+    document.querySelector(element_ID).textContent = "";
+}
+
+function removeBook(myLibrary, book_index) {
+    myLibrary.splice(book_index, 1);
+}
+
+displayBooks(myLibrary);
+
 document.querySelector("#show-btn").addEventListener('click', function(e) {
     e.preventDefault();
-    showBookForm();
+    // showBookForm();
 });
 
 document.querySelector("#confirm-btn").addEventListener('click', function(e) {
     e.preventDefault();
     addBookToLibrary(myLibrary);
     clearBox(".book-container");
-    hideBookForm();
+    // hideBookForm();
     displayBooks(myLibrary);
 });
 
-function displayBooks(myLibrary) {
-    console.log("I have " + myLibrary.length + " books");
-    for (i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i];
-        console.log(i, book.name);
-        genBookDiv(".book-container", i);
-        genBookInfo(book, `#book${i}`);
-    }
-}
-
-function genBookDiv(container, book_index) {
-    const book_container = document.querySelector(container);
-    let div = document.createElement("div");
-    div.className = "book";
-    div.id = `book${book_index}`
-    book_container.appendChild(div);
-}
-
-function genBookInfo(book, book_div_ID){
-    const book_div = document.querySelector(book_div_ID);
-    const book_info = [book.name, book.author, book.pages, book.read];
-    for (j = 0; j < 4; j++){
-        let p = document.createElement("p");
-        p.innerHTML = book_info[j];
-        book_div.appendChild(p);
-    }
-}
-
-function clearBox(elementID) {
-    document.querySelector(elementID).innerHTML = "";
-}
-
-displayBooks(myLibrary);
+// const remove_btns = document.querySelectorAll(".remove");
+// for (let i = 0; i < remove_btns.length; i++) {
+//     remove_btns[i].addEventListener('click', function(e) {
+//         e.preventDefault();
+//         removeBook(myLibrary, i);
+//         removeElements(`#book${i}`);
+//         clearBox(".book-container");
+//         displayBooks(myLibrary);
+//     });
+// }
